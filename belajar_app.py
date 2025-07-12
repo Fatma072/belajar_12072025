b3f8242d0cbb5631ce93b631c473edbfe5a0f195
Fatma072 <ftmx.na@gmail.com>
2025-07-12 09:05:27 +0700

    Update belajar_app.py

--- belajar_app.py
+++ belajar_app.py
@@ -8,3 +8,14 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
+
+import streamlit as st
+
+a, b = st.columns(2)
+c, d = st.columns(2)
+
+a.metric("Temperature", "30°F", "-9°F", border=True)
+b.metric("Wind", "4 mph", "2 mph", border=True)
+
+c.metric("Humidity", "77%", "5%", border=True)
+d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
