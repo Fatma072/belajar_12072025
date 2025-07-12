26588ca171d216a068372808009857d4fd953834
Fatma072 <ftmx.na@gmail.com>
2025-07-12 08:47:42 +0700

    Update belajar_app.py

--- belajar_app.py
+++ belajar_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
