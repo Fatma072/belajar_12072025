782979d242e8ab7fae665f7c8d894e15cd3179e0
Fatma072 <ftmx.na@gmail.com>
2025-07-12 09:02:30 +0700

    Update belajar_app.py

--- belajar_app.py
+++ belajar_app.py
@@ -5,3 +5,7 @@ st.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 col1.metric("Temperature", "70 °F", "1.2 °F")
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
