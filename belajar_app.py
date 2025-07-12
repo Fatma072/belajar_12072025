c3d679978bcd700c818084c8693816e51cfbfb39
Fatma072 <ftmx.na@gmail.com>
2025-07-12 09:01:52 +0700

    Update belajar_app.py

--- belajar_app.py
+++ belajar_app.py
@@ -4,3 +4,4 @@ st.write("Hello, *World!* :sunglasses:")
 st.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+col1.metric("Temperature", "70 °F", "1.2 °F")
