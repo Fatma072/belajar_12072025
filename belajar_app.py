f09ebe7a0ecffe159aa164a6a51529d9e294c33c
Fatma072 <ftmx.na@gmail.com>
2025-07-12 09:03:35 +0700

    Update belajar_app.py

--- belajar_app.py
+++ belajar_app.py
@@ -1,14 +1,7 @@
 import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+
 import streamlit as st
 
 col1, col2, col3 = st.columns(3)
